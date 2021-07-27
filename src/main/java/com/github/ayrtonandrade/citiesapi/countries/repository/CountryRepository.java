268ae9b8e0265573fddddff6f34a782089a7b432
kjk154 <ayrton.andrade1@gmail.com>
2021-07-27 08:42:21 -0300

    Bootcamp everis

---
+++ src/main/java/com/github/ayrtonandrade/citiesapi/countries/repository/CountryRepository.java
@@ -0,0 +1,6 @@
+package com.github.ayrtonandrade.citiesapi.countries.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CountryRepository extends JpaRepository<Country, Long> {
+}
